7d54fbae3c5f24afd2c355750879c8db0bbd84fa
孙诚 <cheng2.sun@ly.com>
2020-12-14 16:48:38 +0800

    80

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -20,7 +20,9 @@ namespace BlazorApp1
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder
+                        .UseUrls("http://*:80")
+                        .UseStartup<Startup>();
                 });
     }
 }
